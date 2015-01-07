a4c628d3e0bf87156adce505c9151404ffe4f84c
sue445 <sue445@sue445.net>
2015-01-08 01:13:29 +0900

    Remove japanese comment

--- src/main/java/com/vexus2/jenkins/chatwork/jenkinschatworkplugin/ChatworkPublisher.java
+++ src/main/java/com/vexus2/jenkins/chatwork/jenkinschatworkplugin/ChatworkPublisher.java
@@ -153,7 +153,6 @@ public class ChatworkPublisher extends Publisher {
       int size = commits.size();
       for (int i = 0; i < size; i++) {
         JSONObject value = (JSONObject) commits.get(i);
-        // コミットメッセージが長くなりすぎることを考慮して文字長を50文字とする
         String s = value.getString("message");
         message.append(String.format("- %s\n", (s.length() > 50) ? s.substring(0, 50) + "..." : s));
       }
